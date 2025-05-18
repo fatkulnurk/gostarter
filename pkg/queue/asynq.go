c43c455341516870673d42546510eb4c0049dd5c
fatkulnurk <fatkulnurk@gmail.com>
2025-05-18 16:06:38 +0700

    fix(queue): handle ping error and remove client close- Add error handling for client.Ping() by logging a fatal error if it fails- Comment out the defer client.Close() block to prevent closing the client

--- pkg/queue/asynq.go
+++ pkg/queue/asynq.go
@@ -4,19 +4,21 @@ import (
 	"github.com/fatkulnurk/gostarter/config"
 	"github.com/hibiken/asynq"
 	"github.com/redis/go-redis/v9"
+	"log"
 )
 
 func NewAsynqClient(cfg *config.Queue, redis *redis.Client) (*asynq.Client, error) {
 	client := asynq.NewClientFromRedisClient(redis)
-	defer func(client *asynq.Client) {
-		err := client.Close()
-		if err != nil {
-			panic(err)
-		}
-	}(client)
+	//defer func(client *asynq.Client) {
+	//	err := client.Close()
+	//	if err != nil {
+	//		panic(err)
+	//	}
+	//}(client)
 
 	err := client.Ping()
 	if err != nil {
+		log.Fatal("failed to ping redis: ", err)
 		return nil, err
 	}
 
