5d05f41015831c1a19c6ba3683699f9e8a382e19
fatkulnurk <fatkulnurk@gmail.com>
2025-05-18 14:20:41 +0700

    refactor: remove unused database and redis initialization functions- Removed InitMySQL and InitRedis functions from cmd.go- Removed unused imports related to database/sql and redis- Simplified the file structure for future maintenance

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -1,20 +1,12 @@
 package cmd
 
 import (
-	"database/sql"
 	"flag"
 	"fmt"
 	"magicauth/cmd/http"
 	"magicauth/cmd/worker"
 	"magicauth/config"
 	"os"
-	"strconv"
-	"time"
-
-	"magicauth/pkg/cache"
-	"magicauth/pkg/db"
-
-	"github.com/redis/go-redis/v9"
 )
 
 func ServeApp(svc string, cfg *config.Config) {
@@ -33,47 +25,4 @@ func ServeApp(svc string, cfg *config.Config) {
 		flag.Usage()
 		os.Exit(1)
 	}
-}
-
-func InitMySQL() (*sql.DB, error) {
-	params := os.Getenv("DB_PARAMS")
-	if params == "" {
-		params = "charset=utf8mb4&parseTime=True&loc=Local"
-	}
-
-	port, err := strconv.Atoi(os.Getenv("DB_PORT"))
-	if err != nil {
-		return nil, err
-	}
-
-	conn, err := db.NewMySQL(db.MySQLConfig{
-		User:            os.Getenv("DB_USER"),
-		Password:        os.Getenv("DB_PASSWORD"),
-		Host:            os.Getenv("DB_HOST"),
-		Port:            port,
-		Database:        os.Getenv("DB_DATABASE"),
-		Params:          params,
-		MaxOpenConns:    25,
-		MaxIdleConns:    5,
-		ConnMaxLifetime: 5 * time.Minute,
-		ConnMaxIdleTime: 10 * time.Minute,
-	})
-	if err != nil {
-		return nil, err
-	}
-
-	return conn, nil
-}
-
-func InitRedis() (*redis.Client, error) {
-	return db.NewRedis(cache.RedisConfig{
-		Addr:            os.Getenv("REDIS_ADDR"),
-		Password:        os.Getenv("REDIS_PASSWORD"),
-		DB:              0,
-		PoolSize:        10,
-		MinIdleConns:    5,
-		ConnMaxLifetime: 5 * time.Minute,
-		PoolTimeout:     4 * time.Second,
-		ConnMaxIdleTime: 30 * time.Minute,
-	})
 }
